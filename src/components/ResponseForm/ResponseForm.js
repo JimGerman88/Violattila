7b35677727972c5bd4c67908641d4c419d74420f
VIOLA\csisz <wirsli@gmail.com>
2024-03-29 19:01:04 +0100

    form fix

--- src/components/ResponseForm/ResponseForm.js
+++ src/components/ResponseForm/ResponseForm.js
@@ -1,21 +1,19 @@
 import React from "react";
-import { Container, Ratio } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 const ResponseForm = () => {
   return (
     <section id="response-form">
       <Container>
         <h1 className="text-center">Visszajelzés</h1>
-        <Ratio aspectRatio={1 / 2}>
+        <div className="form">
           <iframe
             title="Respond form"
             src="https://docs.google.com/forms/d/e/1FAIpQLSdgW4Lc4xINBKj_y51r7g-uVjAVuqeSgT5VxinjScS0Q64Wuw/viewform?embedded=true"
-            height="500px"
-            width="500px"
           >
             Betöltés…
           </iframe>
-        </Ratio>
+        </div>
       </Container>
     </section>
   );
